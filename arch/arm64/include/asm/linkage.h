9ef23bcd1f75a6ccc9a4e89c39d0f998df8f358e
qianyi liu <liuqianyi125@gmail.com>
2021-04-21 14:39:25 +0800

    ARM64 PC counter needs 4 bytes aligned

--- arch/arm64/include/asm/linkage.h
+++ arch/arm64/include/asm/linkage.h
@@ -2,21 +2,19 @@
 #define __LINKAGE_H_INCLUDE__
 
 #ifndef __ALIGN
-#define __ALIGN .align 4, 0x90
-#define __ALIGN_STR ".align 4,0x90"
+#define __ALIGN .align 2
 #endif
 
 #ifdef __ASSEMBLY__
 
 #ifndef LINKER_SCRIPT
 #define ALIGN __ALIGN
-#define ALIGN_STR __ALIGN_STR
 #endif
 
 #ifndef ENTRY
 #define ENTRY(name)                                                            \
-	.globl name;                                                           \
-	ALIGN;                                                                 \
+	.global name;                                                       \
+	ALIGN;                                                              \
     name:
 #endif
 
@@ -24,16 +22,12 @@
 #define END(name) .size name, .- name
 #endif
 
-/* If symbol 'name' is treated as a subroutine (gets called, and returns)
- * then please use ENDPROC to mark 'name' as STT_FUNC for the benefit of
- * static analysis tools such as stack depth analyzer.
- */
 #ifndef ENDPROC
 #define ENDPROC(name)                                                          \
-	.type name, @function;                                                 \
+	.type name, %function;                                              \
 	END(name)
 #endif
 
-#endif /* end of __ASSEMBLY */
+#endif /* __ASSEMBLY__ */
 
-#endif /* end of __LINKAGE_H_INCLUDE__ */
+#endif /* __LINKAGE_H_INCLUDE__ */
